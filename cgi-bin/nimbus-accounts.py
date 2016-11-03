aee72989c7742bef80b174f463bbf0789dd20335
Julian Weiss <insanj@dhcp-10-5-10-27.wireless.rochester.edu>
2016-11-03 16:23:02 -0400

    complete Sign Up + Login cgi-bin python impl + thorough error prevention, redesigned layout of site + added Login forms + added confirm password field

--- cgi-bin/nimbus-accounts.py
+++ cgi-bin/nimbus-accounts.py
@@ -9,65 +9,78 @@ import urllib
 
 cgitb.enable()
 
-# create HTML and CSS stub, to be populated with results from database
-#print 'Content-Type: text/html'
-#print
-#print '''<html>
-#	<head>
-#		<title>Nimbus Syllabus</title>
-#		<link rel="stylesheet" type="text/css" href="../style.css">
-#		<link rel="shortcut icon" type="image/png" href="../favicon.png">
-#		<link rel="icon" type="image/png" href="../favicon.png">
-#	</head>
- #   <body>
-  #      <a href="http://nimsyllabus.com"><img src="../colored_icon.png" />
-	#	<h3>Welcome to</h3>
-	#	<h1>Nimbus Syllabus</h1></a>
-#
-#		<hr/>
-#'''
-
 # retrieve form data from GET request
 user_form = cgi.FieldStorage()
 
-username = user_form['username_field'].value
-password = user_form['password_field'].value
-
-# connect to database, if exists
-conn = sqlite3.connect('nimbus.db') # automatically creates file if doesn't exist
-c = conn.cursor()
-
-c.execute('CREATE TABLE IF NOT EXISTS accounts(id INTEGER primary key, username varchar(250), password varchar(250), timestamp datetime)')
-c.execute('SELECT * FROM accounts WHERE username=?', (username,))
-existing_accounts = c.fetchall()
-
-result_string = '<br/><hr/>'
-
-if len(existing_accounts) > 0:
-	result_string += 'Account already exists with username <b>' + username + '</b>.'
+if not 'username_field' in user_form or not 'password_field' in user_form:
+	original_page = urllib.urlopen('http://nimsyllabus.com/index.html')
+	original_page_text = original_page.read()
+	augmented_text = original_page_text.replace('</body>', "Invalid username or password given. Try again please!" + '</body>')
+	print 'Content-Type: text/html\n\n' + augmented_text
 else:
-	current_time = datetime.datetime.now()
-	salt = str(current_time)
-	hasher = hashlib.md5()
-	hasher.update(password)
-	hasher.update(salt)
-	encrypted_password = hasher.hexdigest()
-	c.execute('INSERT INTO accounts (username, password, timestamp) VALUES (?, ?, ?)', (username, encrypted_password, current_time))
-	result_string += 'Created new account with username <b>' + username + '</b>.'
+	username = user_form['username_field'].value
+	password = user_form['password_field'].value
 
-# read original HTML page and insert stylized result_string
-# original_page_request = urllib2.Request()
-original_page = urllib.urlopen('http://nimsyllabus.com/index.html')
-original_page_text = original_page.read()
-augmented_text = original_page_text.replace('</body>', result_string + '</body>')
-print 'Content-Type: text/html\n\n' + augmented_text
+	submit_value = user_form['submit'].value 
+	result_string = '<br/><hr/>'
 
-# complete database connection and HTML/CSS stub
-conn.commit()
-conn.close()
+	# connect to database, if exists
+	conn = sqlite3.connect('nimbus.db') # automatically creates file if doesn't exist
+	c = conn.cursor()
 
-#print '''
-#  </body>
-#</html>
-#'''
+	if submit_value == 'Sign Up':
+		confirm_password = user_form['confirm_password_field'].value
+		if confirm_password != password:
+			result_string += 'Passwords do not match, please try again!'
+		else:
+			c.execute('CREATE TABLE IF NOT EXISTS accounts(id INTEGER primary key, username varchar(250), password varchar(250), timestamp datetime)')
+			c.execute('SELECT * FROM accounts WHERE username=?', (username,))
+			existing_accounts = c.fetchall()
 
+			if len(existing_accounts) > 0:
+				result_string += 'Account already exists with username <b>' + username + '</b>.'
+			else:
+				current_time = datetime.datetime.now()
+				salt = str(current_time)
+				hasher = hashlib.md5()
+				hasher.update(password)
+				hasher.update(salt)
+				encrypted_password = hasher.hexdigest()
+				c.execute('INSERT INTO accounts (username, password, timestamp) VALUES (?, ?, ?)', (username, encrypted_password, current_time))
+				result_string += 'Created new account with username <b>' + username + '</b>.'
+	else:
+		c.execute('SELECT * FROM accounts WHERE username=?', (username,))
+		existing_account = c.fetchone()
+
+		if not existing_account:
+			result_string += 'Account not found.'
+		else:
+			existing_timestamp = existing_account[3]
+			salt = str(existing_timestamp)
+			hasher = hashlib.md5()
+			hasher.update(password)
+			hasher.update(salt)
+			given_encrypted_password = hasher.hexdigest()
+			existing_encrypted_password = existing_account[2]
+
+			if given_encrypted_password == existing_encrypted_password:
+	 			result_string += 'Logged in! Welcome back :)'
+	 		else:
+	 			result_string += 'Incorrect password for account, try again please!'
+
+	# read original HTML page and insert stylized result_string
+	# original_page_request = urllib2.Request()
+	original_page = urllib.urlopen('http://nimsyllabus.com/index.html')
+	original_page_text = original_page.read()
+	augmented_text = original_page_text.replace('</body>', result_string + '</body>')
+	print 'Content-Type: text/html\n\n' + augmented_text
+
+	# complete database connection and HTML/CSS stub
+	conn.commit()
+	conn.close()
+
+	#print '''
+	#  </body>
+	#</html>
+	#'''
+	
