0526ebf346628456626df322c848f7e2062ff644
insanj <insanj@gmail.com>
2016-11-08 12:14:07 -0500

    store account_cookie with username val + path = / + properly read in login + create act + cookie login, added <body onload= reference in index.html to execute js + fixed reading current_time → account_cookie, refactored code to be visually coherent

--- cgi-bin/nimbus-accounts.py
+++ cgi-bin/nimbus-accounts.py
@@ -18,17 +18,14 @@ user_form = cgi.FieldStorage()
 
 submit_value = user_form['submit'].value 
 
-if submit_value == '+ New Group':
+if submit_value == 'Cookie Login':
 	conn = sqlite3.connect('nimbus.db') # automatically creates file if doesn't exist
 	c = conn.cursor()
 	
-	username = 'meow'
-	group_name = user_form['group_name'].value 
-	group_color = user_form['group_color'].value 
+	username = user_form['username'].value
 	current_time = datetime.datetime.now()
 
 	c.execute('CREATE TABLE IF NOT EXISTS groups(id INTEGER primary key, username varchar(250), group_name varchar(250), group_color varchar(250), timestamp datetime)')
-	c.execute('INSERT INTO groups (username, group_name, group_color, timestamp) VALUES (?, ?, ?, ?)', (username, group_name, group_color, current_time))
 	c.execute('SELECT * FROM groups WHERE username=?', (username,))
 
 	result_string = '<br/><br/>'
@@ -44,6 +41,52 @@ if submit_value == '+ New Group':
 	original_page_text = original_page.read()
 	augmented_text = original_page_text.replace('</body>', result_string + '</body>')
 	print 'Content-Type: text/html\n\n' + augmented_text
+elif submit_value == '+ New Group':
+	if not stored_cookie_string:
+		original_page = urllib.urlopen('http://nimsyllabus.com/index.html')
+		original_page_text = original_page.read()
+		augmented_text = original_page_text.replace('</body>', "Cannot find logged in account, please log in again." + '</body>')
+		print 'Content-Type: text/html\n\n' + augmented_text
+	else:		
+		cookie = Cookie.SimpleCookie(stored_cookie_string)
+		if not 'account_cookie' in cookie:	
+			original_page = urllib.urlopen('http://nimsyllabus.com/index.html')
+			original_page_text = original_page.read()
+			augmented_text = original_page_text.replace('</body>', "Cannot find username for current account, please log in again." + '</body>')
+			print 'Content-Type: text/html\n\n' + augmented_text
+		else:
+			username = cookie['account_cookie'].value
+		
+			conn = sqlite3.connect('nimbus.db') # automatically creates file if doesn't exist
+			c = conn.cursor()
+
+			group_name = 'Untitled'
+			if 'group_name' in user_form:
+				group_name = str(user_form['group_name'].value)
+
+			group_color = 'black'
+			if 'group_color' in user_form:
+				group_color = str(user_form['group_color'].value)
+
+			current_time = datetime.datetime.now()
+
+			c.execute('CREATE TABLE IF NOT EXISTS groups(id INTEGER primary key, username varchar(250), group_name varchar(250), group_color varchar(250), timestamp datetime)')
+			c.execute('INSERT INTO groups (username, group_name, group_color, timestamp) VALUES (?, ?, ?, ?)', (username, group_name, group_color, current_time))
+			c.execute('SELECT * FROM groups WHERE username=?', (username,))
+
+			result_string = '<br/><br/>'
+			for group in c:
+				result_group_name = group[2]
+				result_group_color = str(group[3])
+				result_string += '<div class="group" style="color:' + result_group_color + '">' + result_group_name + '</div>'
+
+			conn.commit()
+			conn.close()
+
+			original_page = urllib.urlopen('http://nimsyllabus.com/content.html')
+			original_page_text = original_page.read()
+			augmented_text = original_page_text.replace('</body>', result_string + '</body>')
+			print 'Content-Type: text/html\n\n' + augmented_text
 elif not 'username_field' in user_form or not 'password_field' in user_form:
 	original_page = urllib.urlopen('http://nimsyllabus.com/index.html')
 	original_page_text = original_page.read()
@@ -83,11 +126,10 @@ else:
 				load_content_page = True
 
 				cookie = Cookie.SimpleCookie()
-   				cookie['current_time'] = current_time
-    				cookie['username'] = username
-				token = randint(0,99999999)
-    				cookie['token'] = token
+ 				cookie['account_cookie'] = username
+    				cookie['account_cookie']['path'] = '/'
 				print cookie # important thing
+
 	else:
 		c.execute('SELECT * FROM accounts WHERE username=?', (username,))
 		existing_account = c.fetchone()
@@ -95,8 +137,6 @@ else:
 		if not existing_account:
 			result_string += 'Account not found.'
 		else:
- 			result_string += '<br/><br/>Logged in! Welcome back :)'
-
 			existing_timestamp = existing_account[3]
 			salt = str(existing_timestamp)
 			hasher = hashlib.md5()
@@ -107,6 +147,24 @@ else:
 
 			if given_encrypted_password == existing_encrypted_password:
 				load_content_page = True
+ 				result_string += '<br/><br/>Logged in! Welcome back :)'
+
+ 				cookie = Cookie.SimpleCookie()
+ 				#cookie['path'] = cookie_path
+ 				# cookie['domain'] = 'nimsyllabus.com'
+ 				#cookie['account'] = 'account'
+ 				cookie['account_cookie'] = username
+   				# cookie['account_cookie']['current_time'] = datetime.datetime.now()
+				#token = randint(0,99999999)
+    				#cookie['account_cookie']['token'] = token
+    				cookie['account_cookie']['path'] = '/'
+    				# cookie['account_cookie']['expires'] = ''
+    				# cookie['httponly'] = '0'
+    				#cookie['account_cookie']['domain'] = 'nimsyllabus.com'
+    				#cookie['expires'] = '7' # datetime.date(2020, 4, 20)
+    			#cookie['path'] = "/"
+
+				print cookie # important thing
 
 				c.execute('SELECT * FROM groups WHERE username=?', (username,))
 
